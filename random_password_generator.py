261545f0b4b2f58e0e26240bda6f625fde56bcea
Giorgio Livraghi <giorgio5395@gmail.com>
2022-01-01 21:08:45 +0100

    Add files via upload

---
+++ random_password_generator.py
@@ -0,0 +1,26 @@
+import secrets
+import string
+import hashlib
+
+passlen = 0
+specialcharacters = "!@#$'%^\"&*()[]{}-_"
+
+while passlen < 12:					#setting of the password minimum length
+	print("Insert the password desired length.\n##Minimum 12 characters required##")
+	passlen = int(input())
+	if passlen < 12:
+		print("Too short password!\n")
+source = string.ascii_letters + string.digits + specialcharacters
+password = ''.join(secrets.choice(source) for i in range(passlen))
+
+f = open('pwds.txt', 'r')				#opening of the common password cheat sheet
+lines = f.readlines()
+for line in lines:
+	if  line.rstrip() in password:			#checking if the password contains an easily guessable word of the cheat sheet
+		exit("Password found in the cheat sheet")
+print(password)
+
+var = password.encode('utf-8')
+k = hashlib.sha256(var)
+print(k.hexdigest())
+
