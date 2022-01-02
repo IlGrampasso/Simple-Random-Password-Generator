b792d099d5d475f66428becd42cb69abf936a7a2
Giorgio Livraghi <giorgio5395@gmail.com>
2022-01-02 15:53:11 +0100

    Update random_password_generator.py
    Set the fixed magic number for the minimum password length.
    Replaced of the tabs indentation with four spaces.
    Redefined opening and closure of the .txt password file.
    Added check of user input.

--- random_password_generator.py
+++ random_password_generator.py
@@ -2,22 +2,29 @@ import secrets
 import string
 import hashlib
 
+MINIMUM_PASSWORD_LENGTH = 12
+
 passlen = 0
 specialcharacters = "!@#$'%^\"&*()[]{}-_"
 
-while passlen < 12:					#setting of the password minimum length
-	print("Insert the password desired length.\n##Minimum 12 characters required##")
-	passlen = int(input())
-	if passlen < 12:
-		print("Too short password!\n")
+while passlen < MINIMUM_PASSWORD_LENGTH:					#setting of the password minimum length
+    print("Insert the password desired length.\n##Minimum 12 characters required##")
+    passlen = input()
+    try:
+        int(passlen)
+    except:
+        exit("You have not inserted a number, try another time!")
+    passlen = int(passlen)
+    if passlen < MINIMUM_PASSWORD_LENGTH:
+        print("Too short password!\n")
 source = string.ascii_letters + string.digits + specialcharacters
 password = ''.join(secrets.choice(source) for i in range(passlen))
 
-f = open('pwds.txt', 'r')				#opening of the common password cheat sheet
-lines = f.readlines()
+with open("pwds.txt", "r") as f:				#opening of the common password cheat sheet
+    lines = f.read().splitlines()
 for line in lines:
-	if  line.rstrip() in password:			#checking if the password contains an easily guessable word of the cheat sheet
-		exit("Password found in the cheat sheet")
+    if  line.rstrip() in password:			#checking if the password contains an easily guessable word of the cheat sheet
+        exit("Password found in the cheat sheet")
 print("Password:")
 print(password)
 
